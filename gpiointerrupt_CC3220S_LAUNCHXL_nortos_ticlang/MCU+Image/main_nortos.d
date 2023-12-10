fd20fa4512116738727a0bf6e7bd066c1eb6fa50
Owen Capak <105994722+McCoo1023@users.noreply.github.com>
2023-12-10 12:26:52 -0500

    Add files via upload

---
+++ gpiointerrupt_CC3220S_LAUNCHXL_nortos_ticlang/MCU+Image/main_nortos.d
@@ -0,0 +1,9 @@
+# FIXED
+
+main_nortos.o: ../main_nortos.c \
+ /Users/owen/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/NoRTOS.h \
+ /Users/owen/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/Board.h
+
+/Users/owen/ti/simplelink_cc32xx_sdk_7_10_00_13/kernel/nortos/NoRTOS.h:
+
+/Users/owen/ti/simplelink_cc32xx_sdk_7_10_00_13/source/ti/drivers/Board.h:
